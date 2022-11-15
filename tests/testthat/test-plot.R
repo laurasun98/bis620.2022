8153e2f18df2a04b890c1c5608564607f06663dd
LAURA Sun <laurasun@LAURAs-MacBook-Pro.local>
2022-11-14 20:57:12 -0500

    changes make after running lint_package()

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -2,7 +2,7 @@ test_that(
   "The accel_plot() returns a ggplot object.",
   {
     data(ukb_accel)
-    p =  accel_plot(ukb_accel[1:100,])
+    p <-  accel_plot(ukb_accel[1:100, ])
     expect_true(inherits(p, "gg"))
   }
 )
@@ -19,7 +19,27 @@ test_that(
   "The accel_plot() is correct for time-series data.",
   {
     data(ukb_accel)
-    p = accel_plot(ukb_accel[1:100,])
+    p <- accel_plot(ukb_accel[1:100, ])
     expect_doppelganger("first-100-samples", p)
   }
+)
+
+test_that(
+  "we get a data frame for spectral_signature.",
+  {
+    data(ukb_accel)
+    p <- spectral_signature(ukb_accel[1:100, ])
+    expect_true(inherits(p, "data.frame"))
+  }
+
+)
+
+test_that(
+  "we get a data frame for spectral_signature, case when take_log=True.",
+  {
+    data(ukb_accel)
+    p <- spectral_signature(ukb_accel[1:100, ], take_log = TRUE)
+    expect_true(inherits(p, "data.frame"))
+  }
+
 )
