f486de5e5a628c3ff631f9433338a0991b2afbcf
LAURA Sun <laurasun@LAURAs-MacBook-Pro.local>
2022-11-15 15:35:10 -0500

    in class change

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -20,7 +20,7 @@ test_that(
   {
     data(ukb_accel)
     p <- accel_plot(ukb_accel[1:100, ])
-    expect_doppelganger("first-100-samples", p)
+    vdiffr::expect_doppelganger("first-100-samples", p)
   }
 )
 
